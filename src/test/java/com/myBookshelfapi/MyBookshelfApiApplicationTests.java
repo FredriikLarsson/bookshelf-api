98bb45ecdd35767af63edfa51e6cbaa507c2ea33
OptiUserX <62377832+OptiUserX@users.noreply.github.com>
2021-02-22 20:22:27 +0100

    first commit

---
+++ src/test/java/com/myBookshelfapi/MyBookshelfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myBookshelfapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBookshelfApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
